9acdda04742570b15cb0face6d0fc9283857b796
Anna N Helms <unicornbuddha@gmail.com>
2023-10-17 18:18:56 -0700

    Module practice

---
+++ ruby-practice/email_reportable.rb
@@ -0,0 +1,10 @@
+module EmailReportable
+    def send_report
+        puts "Sending email...."
+        puts "email sent!"
+    end
+
+    def generate_report
+        puts "Time to send email!"
+    end
+end
